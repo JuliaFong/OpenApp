fc76fbc536ed78bdfb0d3dc03e51b0564c46181d
Julia Fong <Juliamzfong@gmail.com>
2020-12-11 19:28:00 -0600

    Add two ruby folders

---
+++ Ruby/variables.rb
@@ -0,0 +1,33 @@
+#  variables
+
+
+# assign a value to a variable
+# a string
+location = "SF"
+puts location
+
+my_num = 42
+my_num + 99
+puts my_num
+puts my_num + 78
+
+
+#  a variable is like a placeholder
+
+count = 10
+count = count + 1
+puts count
+
+# counts += 1 is the same as count = count + 1
+
+counts = 20
+counts +=  1
+puts counts
+
+num = 12
+is_even = num % 2 == 0
+puts is_even
+
+num = 40
+num + 5
+puts num
