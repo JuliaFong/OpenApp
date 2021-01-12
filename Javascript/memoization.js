ea937b2007e4efad1a59468eee6a0a0784c845eb
Julia Fong <Juliamzfong@gmail.com>
2021-01-11 23:21:12 -0600

    Update memo notes

--- Javascript/memoization.js
+++ Javascript/memoization.js
@@ -17,6 +17,31 @@ function factorial(n) {
   return ans;
 }
 
-console.log(factorial(6)) // 6 steps 
-console.log(memo)
+// console.log(factorial(6)) // 6 steps 
+// console.log(memo)
 // console.log(factorial(100))
+
+// unoptimized solution
+//O(2^n)
+// function fib(n) {
+//     if (n === 1 || n === 2) return 1
+//     return fib(n-1) + fib(n - 2)
+// }
+
+// console.log(fib(50)) // => 8
+
+// memoized version
+// can have impact on our code
+// linear time complexity
+// O(n)
+function fibo(n, memo = {}) {
+    if (n in memo) return memo[n]
+    if (n === 1 || n === 2) return 1
+
+    // if the arg is not in the memo, calculate it recursice
+    // but save it in the memo for the future
+    memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo)
+    return memo[n]
+}
+
+console.log(fibo(60))
