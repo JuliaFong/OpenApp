82f7b7de39a6b69fafe262161fc030a181cc0043
Julia Fong <Juliamzfong@gmail.com>
2021-01-25 16:41:55 -0600

    Finish insertion sort notes

--- DataAlgorithms/Notes/insertionSortNotes.js
+++ DataAlgorithms/Notes/insertionSortNotes.js
@@ -47,3 +47,40 @@ function insertionSort(arr) {
 // I did say we were going to level up a bit! Some students will grasp this algorithm quickly, and others will need more time. If you are currently scratching your head, that is perfectly okay because when this one clicks, it clicks for good.
 
 // If you're struggling, I cannot emphasize enough how helpful it will be to take out a pen and paper or grab a whiteboard marker and step through the solution provided above one step at a time. Keep track of i, j, currElement, arr[j], and the input arr itself at every step. 
+
+// Time Complexity
+// Insertion Sort runtime is O(n2) because:
+
+// In the worst case scenario where our input array is entirely unsorted, since this algorithm contains a nested loop, its run time behaves similarly to bubbleSort and selectionSort. In this case, we are forced to make a comparison at each iteration of the inner loop. Not convinced? Let's derive the complexity. We'll use much of the same argument as we did in selectionSort. Say we had the worst case scenario where are input array is sorted in full decreasing order, but we wanted to sort it in increasing order:
+
+// n is the length of the input array
+// The outer loop i contributes O(n) in isolation, this is plain to see
+// The inner loop j is more complicated. We know j will iterate until it finds an appropriate place to insert the currElement into the sorted region. However, since we are discussing the case where the data is already in decreasing order, the element must travel the maximum distance to find it's insertion point! We know this insertion point to be index 0, since every currElement will be the next smallest of the array. So:
+// the 1st element travels 1 distance to be inserted
+// the 2nd element travels 2 distance to be inserted
+// the 3rd element travels 3 distance to be inserted
+// ...
+// the n-1th element travels n-1 distance to be inserted
+// This means that our inner loop j will contribute roughly O(n / 2) on average
+// The two loops are nested so our total time complexity is O(n * n / 2) = O(n2)
+
+// Space Complexity
+// O(1)
+
+// The amount of memory consumed by the algorithm does not increase relative to the size of the input array. We use the same amount of memory and create the same amount of variables regardless of the size of our input. A quick indicator of this is the fact that we don't create any arrays.
+
+// When should we use Insertion Sort?
+// "Online Algorithms": the input data is streamed in pieces
+// Insertion Sort has one advantage that makes it absolutely supreme in one special case. Insertion Sort is what's known as an "online" algorithm. Online algorithms are great when you're dealing with streaming data, because they can sort the data live as it is received.
+
+// If you must sort a set of data that is ever-incoming, for example, maybe you are sorting the most relevant posts in a social media feed so that those posts that are most likely to impact the site's audience always appear at the top of the feed, an online alogirthm like Insertion Sort is a great option.
+
+// Insertion Sort works well in this situation because the left side of the array is always sorted, and in the case of nearly sorted arrays, it can run in linear time. The absolute best case scenario for Insertion Sort is when there is only one unsorted element, and it is located all the way to the right of the array.
+
+// Well, if you have data constantly being pushed to the array, it will always be added to the right side. If you keep your algorithm constantly running, the left side will always be sorted. Now you have linear time sort.
+
+// Else:
+// Insertion Sort is, in general, useful in all the same situations as Bubble Sort. It's a good option when:
+
+// You are sorting really small arrays where run time will be negligible no matter what algorithm we choose.
+// You are sorting an array that you expect to already be nearly sorted.
