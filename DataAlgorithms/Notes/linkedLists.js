de6b6ff2f0b56f70c8a4bc83887a095bd869de4c
Julia Fong <Juliamzfong@gmail.com>
2021-02-20 19:23:14 -0600

    Add linked list notes

---
+++ DataAlgorithms/Notes/linkedLists.js
@@ -0,0 +1,32 @@
+// Linked Lists
+//  Linked list don't have the most practical real-world applictions,
+// they're important and effective education tool on what data
+
+// Linked list are simple
+// They can be applied as the underlying data structure while implementing
+// a variety of other prevalent abstract data types such as Lists, Stacks, and queues. 
+
+
+// A linked list data structure represents a linear sequence of vertices and tracks three important properties
+
+// Linked List Properties 
+
+//  Property: Head
+// Description: The first node in the list
+
+// Property: Tail
+// Description: The last node in the list
+
+// Property: Length
+// Description: The number of nodes in the listl the list's length.
+
+// Data being tracked by a particular Linked List does not live inside the 
+// linked list instance itself. 
+
+// Node instances track important properties as well
+
+// Linked List Node Properties:
+
+// Property: Value
+// Description: The actual value this node represents
+
