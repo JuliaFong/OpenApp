584c7c59be1516e723bc5749fd4374750c5dfd7c
Julia Fong <Juliamzfong@gmail.com>
2021-03-22 20:55:23 -0500

    Add stack and queue notes

---
+++ DataAlgorithms/Notes/stacksAndQueuesNotes.js
@@ -0,0 +1,31 @@
+// STACKS AND QUEUES
+
+// Stakc and queues are really "data structures"
+
+// The most appropriate terminology would be to call them abstract data types(ADTs)
+// their definitions are more conceptual and related to the rules governing their
+// user-facing behavrios rather than their core imprementations
+
+// Stacks and Queues represent a linear collection of nodes or values
+
+// they are similar to linked list data structure discussed in the previous section
+
+// WHAT IS A STACK
+
+// Stacks are a Last In First Out (LIFO) data structure
+// the last node added to a stack is always the first node to be removed,
+//  and as a result, the first node added 
+// is always the last node removed
+
+// We add this to the top of a stack
+// We remove things from the top of the stack
+
+// We can use JS arrays to implement a basi stack
+
+// Array#push => Add to the top of the stack
+// Arrayp#pop => will remove from the top of the stack
+
+// Queues are a First In First Out (FIFO) data structure
+// The first Node added to the queue is always the first Node to be removed
+
+
